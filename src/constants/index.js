3057faa66c80670e4694194519b76c067a2a4416
deveshtuteja <deveshtuteja17@gmail.com>
2024-09-15 21:18:01 +0530

    built about section and section wrapper

--- src/constants/index.js
+++ src/constants/index.js
@@ -46,15 +46,15 @@ const services = [
         icon: web,
     },
     {
-        title: "React Native Developer",
+        title: "React.Js Developer",
         icon: mobile,
     },
     {
-        title: "Backend Developer",
+        title: "Full Stack Enthusiast",
         icon: backend,
     },
     {
-        title: "Content Creator",
+        title: "Tech Community Leader",
         icon: creator,
     },
 ];
